59218d42339500f758cd452832adeb2dc45ad74d
zoharri <zohar.rimon@campus.technion.ac.il>
2022-07-09 13:14:38 +0300

    Added relu layers

--- HAND/predictors/predictor.py
+++ HAND/predictors/predictor.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+import torch.nn.functional as F
 from typing import List
 from abc import ABC, abstractmethod
 
@@ -36,20 +37,22 @@ class HAND3x3Predictor(HANDPredictorBase):
 
     def __init__(self, cfg: HANDConfig):
         super().__init__(cfg)
-        self.layers = self._construct_layers()
-
-    def _construct_layers(self):
-        input_dim = 80 * 3 * 2  # TODO: determine from config that also determines PEs (when we agree on an implementation)
         hidden_size = 30
+        self.layers = self._construct_layers(hidden_size)
+        self.final_linear_layer = nn.Linear(hidden_size, 9)
+
+    def _construct_layers(self, hidden_size):
+        input_dim = 80 * 3 * 2  # TODO: determine from config that also determines PEs (when we agree on an implementation)
         blocks = [nn.Linear(input_dim, hidden_size)]
         blocks.extend([nn.Linear(hidden_size, hidden_size) for _ in range(self.cfg.num_blocks - 2)])
-        blocks.append(nn.Linear(hidden_size, 9))  # last layer predicts 3x3 output
         return nn.ModuleList(blocks)
 
     def forward(self, positional_embedding: torch.Tensor) -> torch.Tensor:
         x = positional_embedding
         for layer in self.layers:
             x = layer(x)
+            x = F.relu(x)
+        x = self.final_linear_layer(x)
         return x
 
 
