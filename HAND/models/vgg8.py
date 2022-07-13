17b43c0278091d9daa2c6a19357b44508ba1cc06
zoharri <zohar.rimon@campus.technion.ac.il>
2022-07-13 11:07:23 +0300

    Created vgg8 architecture

---
+++ HAND/models/vgg8.py
@@ -0,0 +1,95 @@
+from typing import List, Tuple
+
+import torch
+from torch import nn as nn
+from torch.nn import functional as F
+
+from HAND.models.model import OriginalModel, ReconstructedModel
+from HAND.options import EmbeddingsConfig
+from HAND.positional_embedding import MyPositionalEncoding
+
+
+class VGG8(OriginalModel):
+    def __init__(self, input_size: int, is_rgb: bool = True, num_classes: int = 10):
+        super(VGG8, self).__init__()
+
+        self.input_size = input_size
+        self.num_classes = num_classes
+        self.input_channels = 3 if is_rgb else 1
+        self.conv_layer_channels = [self.input_channels, 6, 16, 32, 64, 64]
+        self.num_conv_layers = len(self.conv_layer_channels)
+        self.pooling_factor = 2 ** self.num_conv_layers
+        self.fc_layer_channels = [(self.input_size // self.pooling_factor) ** 2 * self.conv_layer_channels[-1], 128,
+                                  128, self.num_classes]
+        self.num_fc_layers = len(self.fc_layer_channels)
+        self.layers_list = [
+            nn.Conv2d(self.conv_layer_channels[i], self.conv_layer_channels[i + 1], (3, 3), (1, 1), padding='same') for
+            i in range(self.num_conv_layers - 1)]
+        self.conv_layers = nn.ModuleList(self.layers_list)
+
+        self.fc_layers = [nn.Linear(self.fc_layer_channels[i], self.fc_layer_channels[i + 1]) for i in
+                          range(self.num_fc_layers - 1)]
+        self.fc_layers = nn.ModuleList(self.fc_layers)
+        self.feature_maps = []
+
+    def get_feature_maps(self, batch: torch.Tensor) -> Tuple[torch.Tensor, List[torch.Tensor]]:
+        self.feature_maps = []
+        output = self.forward(batch, extract_feature_maps=True)
+        return output, self.feature_maps
+
+    def get_learnable_weights(self):
+        tensors = []
+        for conv_layer in self.conv_layers:
+            tensors.append(conv_layer.weight)
+        return tensors
+
+    def get_fully_connected_weights(self) -> List[torch.Tensor]:
+        tensors = []
+        for fc_layer in self.fc_layers:
+            tensors.append(fc_layer.weight)
+        return tensors
+
+    def forward(self, x, extract_feature_maps=False):
+        for conv_layer in self.conv_layers:
+            x = conv_layer(x)
+            if extract_feature_maps:
+                self.feature_maps.append(x)
+            x = F.relu(x)
+            # x = F.max_pool2d(x, 2)
+        x = torch.flatten(x, 1)
+        for fc_layer in self.fc_layers:
+            x = fc_layer(x)
+        output = F.log_softmax(x, dim=1)
+        return output
+
+
+class ReconstructedSimpleNet3x3(ReconstructedModel):
+    def __init__(self, original_model: VGG8, embeddings_cfg: EmbeddingsConfig):
+        super().__init__(original_model)
+        self.indices = self._get_tensor_indices()
+        self.positional_encoder = MyPositionalEncoding(embeddings_cfg)
+        self.positional_embeddings = self._calculate_position_embeddings()
+
+    def _get_tensor_indices(self) -> List[List[Tuple]]:
+        indices = []
+        curr_layer_indices = []
+        for filter_idx in range(self.original_model.num_hidden[0]):
+            curr_layer_indices.append((0, filter_idx, 0))  # Layer 0, Filter i, Channel 0 (in_channels=1)
+        indices.append(curr_layer_indices)
+
+        for layer_idx in range(1, len(self.original_model.get_learnable_weights())):
+            curr_layer_indices = []
+            for filter_idx in range(self.original_model.num_hidden[layer_idx]):
+                for channel_idx in range(self.original_model.num_hidden[layer_idx - 1]):
+                    curr_layer_indices.append((layer_idx, filter_idx, channel_idx))
+            indices.append(curr_layer_indices)
+
+        return indices
+
+    def _calculate_position_embeddings(self) -> List[List[torch.Tensor]]:
+        positional_embeddings = [[self.positional_encoder(idx) for idx in layer_indices] for layer_indices in
+                                 self.indices]
+        return positional_embeddings
+
+    def get_indices_and_positional_embeddings(self) -> Tuple[List[List[Tuple]], List[List[torch.Tensor]]]:
+        return self.indices, self.positional_embeddings
