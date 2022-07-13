3681099e3bd2867e62b9f03cd24547188d40963b
zoharri <zohar.rimon@campus.technion.ac.il>
2022-07-13 15:58:44 +0300

    Added resnet18 arch

---
+++ HAND/models/resnet18.py
@@ -0,0 +1,116 @@
+from typing import List, Tuple
+
+import torch
+import torchvision.models
+from torch import nn as nn
+from torch.nn import functional as F
+
+from HAND.models.model import OriginalModel, ReconstructedModel
+from HAND.options import EmbeddingsConfig
+from HAND.positional_embedding import MyPositionalEncoding
+
+
+class ResNet18(OriginalModel):
+    def __init__(self, num_classes: int = 10):
+        super(ResNet18, self).__init__()
+
+        self.model = torchvision.models.resnet18(pretrained=False)
+        self.layers_names = ['layer1', 'layer2', 'layer3', 'layer4']
+        self.feature_maps = []
+        self.num_classes = num_classes
+        self.model.fc.weight = torch.nn.Linear(self.model.fc.in_features, self.num_classes)
+
+
+    def get_feature_maps(self, batch: torch.Tensor) -> Tuple[torch.Tensor, List[torch.Tensor]]:
+        self.feature_maps = []
+        output = self.forward(batch, extract_feature_maps=True)
+        return output, self.feature_maps
+
+    def get_learnable_weights(self):
+        tensors = []
+        for layer_name in self.layers_names:
+            module = self.model._modules[layer_name]
+            for block in module.modules():
+                tensors.append(block.conv1.weight)
+                tensors.append(block.conv2.weight)
+        return tensors
+
+    def get_fully_connected_weights(self) -> List[torch.Tensor]:
+        tensors = [self.model.fc]
+        return tensors
+
+    def layer_forward(self, layer: torch.nn.Sequential, x: torch.Tensor, extract_feature_maps: bool = False) -> (
+            torch.Tensor, List[torch.Tensor]):
+
+        activations = []
+        identity = x
+
+        out = layer.conv1(x)
+        out = layer.bn1(out)
+        if extract_feature_maps is True:
+            activations.append(x)
+        out = layer.relu(out)
+
+        out = layer.conv2(out)
+        out = layer.bn2(out)
+        if extract_feature_maps is True:
+            activations.append(x)
+
+        if layer.downsample is not None:
+            identity = layer.downsample(x)
+
+        out += identity
+        out = layer.relu(out)
+
+        return out, activations
+
+    def forward(self, x, extract_feature_maps=False):
+        # See note [TorchScript super()]
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x, layer1_activations = self.layer_forward(self.layer1, x, extract_feature_maps)
+        x, layer2_activations = self.layer_forward(self.layer2, x, extract_feature_maps)
+        x, layer3_activations = self.layer_forward(self.layer3, x, extract_feature_maps)
+        x, layer4_activations = self.layer_forward(self.layer4, x, extract_feature_maps)
+        activations = layer1_activations + layer2_activations + layer3_activations + layer4_activations
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+        if extract_feature_maps is True:
+            self.feature_maps = activations
+        return x
+
+
+class ReconstructedVGG3x3(ReconstructedModel):
+    def __init__(self, original_model: VGG8, embeddings_cfg: EmbeddingsConfig):
+        super().__init__(original_model)
+        self.indices = self._get_tensor_indices()
+        self.positional_encoder = MyPositionalEncoding(embeddings_cfg)
+        self.positional_embeddings = self._calculate_position_embeddings()
+
+    def _get_tensor_indices(self) -> List[List[Tuple]]:
+        indices = []
+        curr_layer_indices = []
+        for filter_idx in range(self.original_model.num_hidden[0]):
+            curr_layer_indices.append((0, filter_idx, 0))  # Layer 0, Filter i, Channel 0 (in_channels=1)
+        indices.append(curr_layer_indices)
+
+        for layer_idx in range(1, len(self.original_model.get_learnable_weights())):
+            curr_layer_indices = []
+            for filter_idx in range(self.original_model.num_hidden[layer_idx]):
+                for channel_idx in range(self.original_model.num_hidden[layer_idx - 1]):
+                    curr_layer_indices.append((layer_idx, filter_idx, channel_idx))
+            indices.append(curr_layer_indices)
+
+        return indices
+
+    def _calculate_position_embeddings(self) -> List[List[torch.Tensor]]:
+        positional_embeddings = [[self.positional_encoder(idx) for idx in layer_indices] for layer_indices in
+                                 self.indices]
+        return positional_embeddings
+
+    def get_indices_and_positional_embeddings(self) -> Tuple[List[List[Tuple]], List[List[torch.Tensor]]]:
+        return self.indices, self.positional_embeddings
