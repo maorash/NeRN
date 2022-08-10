1160e82e7ccdf8be5e723a958f0575f112d93e8a
Maor <maorashkenazi@gmail.com>
2022-08-10 14:44:41 +0300

    Fixed bug in pretrained cifar10 original task

--- HAND/tasks/cifar10/cifar10_akamaster_train.py
+++ HAND/tasks/cifar10/cifar10_akamaster_train.py
@@ -104,11 +104,12 @@ def main():
         if os.path.isfile(args.resume):
             print("=> loading checkpoint '{}'".format(args.resume))
             checkpoint = torch.load(args.resume)
-            args.start_epoch = checkpoint['epoch']
-            best_prec1 = checkpoint['best_prec1']
-            model.load_state_dict(checkpoint['state_dict'])
-            print("=> loaded checkpoint '{}' (epoch {})"
-                  .format(args.evaluate, checkpoint['epoch']))
+            # args.start_epoch = checkpoint['epoch']
+            # best_prec1 = checkpoint['best_prec1']
+            model.module.load_state_dict(checkpoint)
+            print("=> loaded checkpoint '{}".format(args.evaluate))
+            # print("=> loaded checkpoint '{}' (epoch {})"
+            #       .format(args.evaluate, checkpoint['epoch']))
         else:
             print("=> no checkpoint found at '{}'".format(args.resume))
 
